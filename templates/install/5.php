da7e75cc315da6c3f52d95a16ff0cbf5ee357434
Hristiyan Ivanov <hrisivanov2@gmail.com>
2019-10-25 18:49:19 +0300

    Remove empty href

--- templates/install/5.php
+++ templates/install/5.php
@@ -3,7 +3,7 @@
                             <p class="card-text">{{$config.name}} will need it's database populated. Please run all migrations.</p>
                             <form class="form" id="database-form" method='post' action="?step=6">
                                 <pre><p id="result" class="text-success"></p></pre>
-                                <button type="submit" href="" id="fifth-step" class="btn btn-back">Run <i class="fas fa-database"></i> & Continue <i class="fas fa-arrow-circle-right"></i></button>
+                                <button type="submit" id="fifth-step" class="btn btn-back">Run <i class="fas fa-database"></i> & Continue <i class="fas fa-arrow-circle-right"></i></button>
                             </form>
                             <div>
                         </div>
